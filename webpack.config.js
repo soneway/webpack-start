2953f506a42c29fb8a711f4cf94301a5881786f7
宋均均 <songjunjun@bytedance.com>
2021-04-08 15:04:09 +0800

    feat: sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  devtool: false,
+  devtool: 'inline-source-map',
   mode: 'development',
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
+    // print: './src/print.js',
   },
   output: {
     filename: '[name].bundle.js',
@@ -21,13 +21,13 @@ module.exports = {
           {
             loader: path.resolve(__dirname, 'build/loaders/minify-loader.js'),
             options: {
-              minify: true,
+              minify: false,
             },
           },
           {
             loader: path.resolve(__dirname, 'build/loaders/comments-loader.js'),
             options: {
-              removeComments: true,
+              removeComments: false,
             },
           },
         ],
